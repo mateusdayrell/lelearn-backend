6b3ad14b3ba2297c3bcaed7702b553d9ce97a0ad
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-09 12:35:56 -0300

    feat: adicionar ponto final nas mensagens de validacao no model Treinamento

--- src/models/Treinamento.js
+++ src/models/Treinamento.js
@@ -6,19 +6,20 @@ class Treinamento extends Model {
       { // init Model
         cod_treinamento: {
           type: Sequelize.STRING(4),
+          defaultValue: '',
           allowNull: false,
-          primaryKey: true,
           len: {
             args: [4, 4],
-            msg: 'O código do treinamento deve ter 4 caracteres',
+            msg: 'O código do treinamento deve ter 4 caracteres.',
           },
         },
         nome_treinamento: {
           type: Sequelize.STRING(30),
+          defaultValue: '',
           allowNull: false,
           len: {
             args: [3, 30],
-            msg: 'O nome do treinamento deve ter entre 3 e 30 caracteres',
+            msg: 'O nome do treinamento deve ter entre 3 e 30 caracteres.',
           },
         },
         desc_treinamento: {
@@ -26,7 +27,7 @@ class Treinamento extends Model {
           allowNull: true,
           len: {
             args: [0, 150],
-            msg: 'O código do treinamento deve ter no máximo 150 caracteres',
+            msg: 'O código do treinamento deve ter no máximo 150 caracteres.',
           },
         },
       },
