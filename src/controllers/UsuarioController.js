4c36b7eec0ac987ed3610e8630d4b31991ca804d
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-10 11:01:27 -0300

    feat: adicionar CRUD de usuarios em UsuarioController

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -2,17 +2,108 @@ import Usuario from '../models/Usuario';
 
 class UsuarioController {
   async index(req, res) {
-    const novoUsuario = await Usuario.create({
-      cpf: '00000000000',
-      nome: 'nome',
-      email: 'email@email.com',
-      telefone: '38999999999',
-      password: '12345678',
-      tipo: 0,
-    });
-    res.json(
-      novoUsuario,
-    );
+    try {
+      const usuarios = await Usuario.findAll();
+
+      return res.json(usuarios);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const usuario = await Usuario.findByPk(id);
+
+      return res.json(usuario);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // async create(req, res) {
+  //   try {
+
+  //   } catch (error) {
+
+  //   }
+  // }
+
+  async store(req, res) {
+    try {
+      const novoUsuario = await Usuario.create(req.body);
+
+      return res.json(novoUsuario);
+    } catch (error) {
+      return res.status(400).json({
+        erros: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // async edit(req, res) {
+  //   try {
+
+  //   } catch (error) {
+
+  //   }
+  // }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          erros: ['CPF não enviado.'],
+        });
+      }
+
+      const usuario = await Usuario.findByPk(id);
+
+      if (!usuario) {
+        return res.status(400).json({
+          erros: ['Usuário não existe.'],
+        });
+      }
+
+      const usuarioEditado = await usuario.update(req.body);
+
+      return res.json(usuarioEditado);
+    } catch (error) {
+      return res.status(400).json({
+        erros: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          erros: ['CPF não enviado.'],
+        });
+      }
+
+      const usuario = await Usuario.findByPk(id);
+
+      if (!usuario) {
+        return res.status(400).json({
+          erros: ['Usuário não existe.'],
+        });
+      }
+
+      await usuario.destroy();
+
+      return res.json(usuario); // também pode enviar null
+    } catch (error) {
+      return res.status(400).json({
+        erros: error.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
