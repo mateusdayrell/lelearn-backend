a8e7eee203820c878ed574e04e260a300990c569
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-10 11:03:06 -0300

    feat: adicionar model de usuario em database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Treinamento from '../models/Treinamento';
+import Usuario from '../models/Usuario';
 
-const models = [Treinamento];
+const models = [Treinamento, Usuario];
 
 const connection = new Sequelize(databaseConfig);
 
