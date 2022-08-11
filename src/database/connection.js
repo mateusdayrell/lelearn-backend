45933fab4e8922857d73548f365bd235b80495ba
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-11 11:36:00 -0300

    feat: adicionar video e curo na conexao com o bd

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,9 +2,12 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Treinamento from '../models/Treinamento';
 import Usuario from '../models/Usuario';
+import Curso from '../models/Curso';
+import Video from '../models/Video';
 
-const models = [Treinamento, Usuario];
+const models = [Treinamento, Usuario, Curso, Video];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
