ef6b1b50aaa8b5f63f28e4e5f9a46f04ecfe76ea
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-15 12:37:37 -0300

    feat: add migration treinamentos_usuarios

---
+++ src/database/migrations/20220815145744-treinamentos_usuarios.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('treinamentos_usuarios', {
+      cod_treinamento: {
+        type: Sequelize.STRING(4),
+        allowNull: false,
+        references: {
+          model: 'treinamentos',
+          key: 'cod_treinamento',
+        },
+        primaryKey: true,
+      },
+      cpf: {
+        type: Sequelize.STRING(11),
+        allowNull: false,
+        references: {
+          model: 'usuarios',
+          key: 'cpf',
+        },
+        primaryKey: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('treinamentos_usuarios');
+  },
+};
