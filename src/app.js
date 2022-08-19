ee436a2cc5fbffbe85322eed015dff910728009b
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-19 07:47:44 -0300

    feat: add cors and helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
 import routes from './routes/routes';
 import './database/connection'; // connect to database;
 
 dotenv.config();
+
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, calback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      calback(null, true);
+    } else {
+      calback(new Error('Não permitido pelo CORS.'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -13,6 +29,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
   }
