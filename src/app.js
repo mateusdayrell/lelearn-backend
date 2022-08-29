f59447f67c2bbf8e67ef6fd9c26ec0bbfdc90a68
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-08-29 14:51:16 -0300

    feat: coment delay

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ class App {
     this.app.use(helmet());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(delay(2000));
+    // this.app.use(delay(2000));
   }
 
   routes() {
