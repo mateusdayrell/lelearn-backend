e7743ef2ce96bcdeb6abd93c57a9278278a0e2c3
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-10-04 14:10:50 -0300

    feat: add nome_aruivo on cursoSeeder

--- src/database/seeds/20220827192214-cursoSeeder.js
+++ src/database/seeds/20220827192214-cursoSeeder.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
@@ -7,6 +8,7 @@ module.exports = {
           cod_curso: '0001',
           nome_curso: 'Desenvolvimento web básico',
           desc_curso: 'Neste curso você vai aprender tudo aquilo que é necessário para iniciar no desenvolvimento web, mesmo sem nenhum conhecimento prévio na área.',
+          nome_arquivo: '1664570936837_1209.jpg',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -14,6 +16,7 @@ module.exports = {
           cod_curso: '0002',
           nome_curso: 'Curso completo NodeJS',
           desc_curso: 'Neste curso você vai aprender a criar aplicações utilizando o NodeJS na prática.',
+          nome_arquivo: '1664570936837_1209.jpg',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -21,6 +24,7 @@ module.exports = {
           cod_curso: '0003',
           nome_curso: 'Curso React Native',
           desc_curso: 'Neste curso você vai aprender a criar aplicações mobile tanto para Android quanto IOS através utilizando o React Native.',
+          nome_arquivo: '1664570936837_1209.jpg',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -28,6 +32,7 @@ module.exports = {
           cod_curso: '0004',
           nome_curso: 'Curso Laravel',
           desc_curso: 'Neste curso você vai aprender todos os fundamentos do Laravel, além de construir um projeto prático utilizando esse framework.',
+          nome_arquivo: '1664570936837_1209.jpg',
           created_at: new Date(),
           updated_at: new Date(),
         },
@@ -35,6 +40,7 @@ module.exports = {
           cod_curso: '0005',
           nome_curso: 'Curso CSS avançado',
           desc_curso: 'Neste curso você aprenderá técnicas avançadas do CSS.',
+          nome_arquivo: '1664570936837_1209.jpg',
           created_at: new Date(),
           updated_at: new Date(),
         },
