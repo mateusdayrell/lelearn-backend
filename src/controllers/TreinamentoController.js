8abb6f323e6cc8b0d1d98d4704092f5a7a69cb00
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-11-11 18:37:10 -0300

    feat: adicionar cursos assistidos na query de getByUsuario em TreinamentoController

--- src/controllers/TreinamentoController.js
+++ src/controllers/TreinamentoController.js
@@ -241,9 +241,13 @@ module.exports = {
       }
 
       const treinamentosUsuarios = await Treinamento.sequelize.query(
-        `SELECT T.cod_treinamento, T.nome_treinamento, TU.prazo, T.desc_treinamento, T.created_at,
+        `SELECT T.cod_treinamento, T.nome_treinamento,
 
-        (SELECT COUNT(UV.cod_video) FROM usuarios_videos UV WHERE UV.cpf = ${id} AND UV.cod_curso IN
+        (SELECT COUNT(T1.cod_curso) FROM treinamentos_cursos T1 WHERE (SELECT COUNT(CV.cod_video)
+          FROM cursos_videos CV WHERE CV.cod_curso = T1.cod_curso) = (SELECT COUNT(UV1.cod_video)
+          FROM usuarios_videos UV1 WHERE UV1.cod_curso = T1.cod_curso AND UV1.cpf = 13211615229)) as cursos_assistidos,
+
+        (SELECT COUNT(UV.cod_video) FROM usuarios_videos UV WHERE UV.cpf = 13211615229 AND UV.cod_curso IN
           (SELECT TC.cod_curso FROM treinamentos_cursos TC WHERE TC.cod_treinamento = T.cod_treinamento))
         as videos_assistidos,
 
@@ -253,7 +257,7 @@ module.exports = {
         FROM treinamentos T, treinamentos_usuarios TU
         WHERE T.daleted_at IS NULL AND
         TU.cod_treinamento = T.cod_treinamento AND
-        TU.cpf = ${id} ORDER BY T.nome_treinamento`,
+        TU.cpf = 13211615229 ORDER BY T.nome_treinamento`,
         { type: QueryTypes.SELECT },
       );
 
