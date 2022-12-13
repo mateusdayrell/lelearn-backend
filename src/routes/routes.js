0a57feb65ac3498a4fe9601348a456faaa9b9647
Mateus Dayrell <mateusdayrell7@gmail.com>
2022-12-13 19:04:33 -0300

    feat: adicionar middlware nas rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -24,67 +24,68 @@ router.post('/send-reset-password/:cpf', tokenController.resetPassword);
 
 // USUÁRIOS
 router.get('/usuarios/', loginRequired, usuarioController.index);
-router.get('/usuarios/:id', usuarioController.show);
-router.get('/usuarios/search/:search', usuarioController.search);
-router.get('/usuarios/get-cursos/:id', usuarioController.getCursos);
-router.get('/usuarios/get-treinamentos/:id', usuarioController.getTreinamentos);
-router.get('/usuarios-videos/:cpf/:cod_curso', usuarioController.getVideos);
-router.post('/usuarios/', usuarioController.store);
-router.put('/usuarios/:id', usuarioController.update);
-router.put('/usuarios/activate/:id', usuarioController.activate);
-router.delete('/usuarios/:id', usuarioController.destroy);
-router.put('/usuarios-videos/:cpf/:cod_curso/:cod_video', usuarioController.updateVideo);
+router.get('/usuarios/:id', loginRequired, usuarioController.show);
+router.get('/usuarios/search/:search', loginRequired, usuarioController.search);
+router.get('/usuarios/get-cursos/:id', loginRequired, usuarioController.getCursos);
+router.get('/usuarios/get-treinamentos/:id', loginRequired, usuarioController.getTreinamentos);
+router.get('/usuarios-videos/:cpf/:cod_curso', loginRequired, usuarioController.getVideos);
+router.post('/usuarios/', loginRequired, usuarioController.store);
+router.put('/usuarios/:id', loginRequired, usuarioController.update);
+router.put('/usuarios/activate/:id', loginRequired, usuarioController.activate);
+router.delete('/usuarios/:id', loginRequired, usuarioController.destroy);
+router.put('/usuarios-videos/:cpf/:cod_curso/:cod_video', loginRequired, usuarioController.updateVideo);
 
 // CURSOS
-router.get('/cursos/', cursoController.index);
-router.get('/cursos/:id', cursoController.show);
-router.get('/cursos/search/:search', cursoController.search);
-router.get('/cursos/get-videos/:id', cursoController.getVideos);
-router.post('/cursos/', cursoController.store);
-router.put('/cursos/:id', cursoController.update);
-router.put('/cursos/activate/:id', cursoController.activate);
-router.delete('/cursos/:id', cursoController.destroy);
+router.get('/cursos/', loginRequired, cursoController.index);
+router.get('/cursos/:id', loginRequired, cursoController.show);
+router.get('/cursos/search/:search', loginRequired, cursoController.search);
+router.get('/cursos/get-videos/:id', loginRequired, cursoController.getVideos);
+router.post('/cursos/', loginRequired, cursoController.store);
+router.put('/cursos/:id', loginRequired, cursoController.update);
+router.put('/cursos/activate/:id', loginRequired, cursoController.activate);
+router.delete('/cursos/:id', loginRequired, cursoController.destroy);
 
 // VIDEOS
-router.get('/videos/', videoController.index);
-router.get('/videos/:id', videoController.show);
-router.get('/videos/get-cursos/:id', videoController.getCursos);
-router.get('/videos/get-by-curso/:cod_curso/:cod_video', videoController.getByCurso);
-router.get('/videos/search/:search', videoController.search);
-router.post('/videos/', videoController.store);
-router.put('/videos/:id', videoController.update);
-router.put('/videos/activate/:id', videoController.activate);
-router.delete('/videos/:id', videoController.destroy);
+router.get('/videos/', loginRequired, videoController.index);
+router.get('/videos/:id', loginRequired, videoController.show);
+router.get('/videos/get-cursos/:id', loginRequired, videoController.getCursos);
+router.get('/videos/get-by-curso/:cod_curso/:cod_video', loginRequired, videoController.getByCurso);
+router.get('/videos/search/:search', loginRequired, videoController.search);
+router.post('/videos/', loginRequired, videoController.store);
+router.put('/videos/:id', loginRequired, videoController.update);
+router.put('/videos/activate/:id', loginRequired, videoController.activate);
+router.delete('/videos/:id', loginRequired, videoController.destroy);
 
 // CURSO-VIDEO
-// router.get('/curso-video/:cod_curso/:cod_video', cursoVideoController.show);
+// router.get('/curso-video/:cod_curso/:cod_video', loginRequired, cursoVideoController.show);
 
 // TREINAMENTOS
-router.get('/treinamentos/', treinamentoController.index);
-router.get('/treinamentos/:id', treinamentoController.show);
-router.get('/treinamentos/search/:search', treinamentoController.search);
-router.get('/treinamentos/get-cursos-usuario/:id/:cpf', treinamentoController.getCursosDoUsuario);
-router.post('/treinamentos/', treinamentoController.store);
-router.put('/treinamentos/:id', treinamentoController.update);
-router.put('/treinamentos/activate/:id', treinamentoController.activate);
-router.delete('/treinamentos/:id', treinamentoController.destroy);
+router.get('/treinamentos/', loginRequired, treinamentoController.index);
+router.get('/treinamentos/:id', loginRequired, treinamentoController.show);
+router.get('/treinamentos/search/:search', loginRequired, treinamentoController.search);
+router.get('/treinamentos/get-cursos-usuario/:id/:cpf', loginRequired, treinamentoController.getCursosDoUsuario);
+router.post('/treinamentos/', loginRequired, treinamentoController.store);
+router.put('/treinamentos/:id', loginRequired, treinamentoController.update);
+router.put('/treinamentos/activate/:id', loginRequired, treinamentoController.activate);
+router.delete('/treinamentos/:id', loginRequired, treinamentoController.destroy);
 
 // COMENTARIOS
-router.get('/comentarios/', comentarioController.index);
-router.get('/comentarios/:id', comentarioController.show);
-router.get('/comentarios/search/:search', comentarioController.search);
-router.post('/comentarios/', comentarioController.store);
-router.put('/comentarios/:id', comentarioController.update);
-router.delete('/comentarios/:id', comentarioController.destroy);
+router.get('/comentarios/', loginRequired, comentarioController.index);
+router.get('/comentarios/:id', loginRequired, comentarioController.show);
+router.get('/comentarios/search/:search', loginRequired, comentarioController.search);
+router.post('/comentarios/', loginRequired, comentarioController.store);
+router.put('/comentarios/:id', loginRequired, comentarioController.update);
+router.put('/comentarios/resolvido/:id', loginRequired, comentarioController.resolve);
+router.delete('/comentarios/:id', loginRequired, comentarioController.destroy);
 
-router.get('/comentarios/root/:cod_video', comentarioController.getRootComments);
-router.get('/comentarios/repplyes/:cod_comentario', comentarioController.getRepplyes);
+router.get('/comentarios/root/:cod_video', loginRequired, comentarioController.getRootComments);
+router.get('/comentarios/repplyes/:cod_comentario', loginRequired, comentarioController.getRepplyes);
 
-router.get('/relatorios/cursos', relatorioController.cursos);
-router.get('/relatorios/videos', relatorioController.videos);
-router.get('/relatorios/treinamento/:id', relatorioController.treinamento);
-router.get('/relatorios/usuario-treinamentos/:id', relatorioController.usuarioTreinamentos);
-router.get('/relatorios/usuario-cursos/:id', relatorioController.usuarioCursos);
+router.get('/relatorios/cursos', loginRequired, relatorioController.cursos);
+router.get('/relatorios/videos', loginRequired, relatorioController.videos);
+router.get('/relatorios/treinamento/:id', loginRequired, relatorioController.treinamento);
+router.get('/relatorios/usuario-treinamentos/:id', loginRequired, relatorioController.usuarioTreinamentos);
+router.get('/relatorios/usuario-cursos/:id', loginRequired, relatorioController.usuarioCursos);
 
 // TREINAMENTOS-USUARIOS
 // router.get('/treinamentos-usuarios/', treinamentoUsuarioController.index);
