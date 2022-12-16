34a875da0c1b63ef9216f9e5eca87c4862e9cec6
mateusdayrell <mateusdayrell7@gmail.com>
2022-12-16 18:05:03 -0300

    modificacoes para vercel

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
   define: {
     timestamps: true,
